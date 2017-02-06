70a21af8cf4b15245facd322439d6378e850ca69
Dan Beil <dbeil@crowdfavorite.com>
2017-02-06 09:21:05 -0600

    initial commit

---
+++ wpcli-migrate-script-source-site.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: WPCLI Migrate Script Source Site
+ * Plugin URI:
+ * Description:
+ * Author: Dan Beil
+ * Version: .0
+ * Author URI: http://addactiondan.me
+ */
+
+
+/**
+ * undocumented class
+ *
+ * @package default
+ * @author
+ **/
+class WPCLI_Migrate_Script_source_Site {
+} // END class
+
+new WPCLI_Migrate_Script_source_Site()
