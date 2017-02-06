241ea61b79977016851341ddf822b6bab9259685
Dan Beil <dbeil@crowdfavorite.com>
2017-02-06 10:42:21 -0600

    Opening nav_menu and nav_menu_items to the REST API
    neither of these are natively open to the API so we must do this ourselves

--- wpcli-migrate-script-source-site.php
+++ wpcli-migrate-script-source-site.php
@@ -12,12 +12,42 @@
 /**
  * undocumented class
  *
+ * http://test.me.dev/wp-json/wp/v2/nav_menu_item
+ *
+ *
+ *
  * @package default
  * @author
  **/
 class WPCLI_Migrate_Script_source_Site {
 
+	public function __construct() {
+		add_action( 'init', array( $this, 'alter_nav_menu_item' ) );
+		add_action( 'init', array( $this, 'alter_nav_menu_tax' ) );
+	}
+
+	/**
+	 * This function opens the individual nav_menu_items to the REST API
+	 *
+	 * Access via http://test.me.dev/wp-json/wp/v2/nav_menu_item
+	 */
+	public function alter_nav_menu_item() {
+		register_post_type( 'nav_menu_item', array(
+			'show_in_rest' => true,
+		) );
+	}
+
+	/**
+	 * This function opens the nav_menu taxonomy to the REST API
+	 *
+	 * Access via http://test.me.dev/wp-json/wp/v2/nav_menu
+	 */
+	public function alter_nav_menu_tax() {
+		register_taxonomy( 'nav_menu', 'nav_menu_item', array(
+			'show_in_rest' => true,
+		) );
+	}
 
 } // END class
 
-new WPCLI_Migrate_Script_source_Site()
+new WPCLI_Migrate_Script_source_Site();
